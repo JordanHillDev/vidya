087bf00600dccc90a0ac2b76ce2ef01426aa14da
Jordanhill90 <74479504+Jordanhill90@users.noreply.github.com>
2023-03-13 19:42:35 -0400

    removed join room button from home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,20 +1,12 @@
-import { useContext, useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useContext } from "react";
 import { Logo } from "../components/common/Logo";
 import { Spinner } from "../components/common/Spinner";
 import CreateRoom from "../components/CreateRoom";
 import { RoomContext } from "../context/RoomContext";
 
 const Home = () => {
-    const [roomId, setRoomId] = useState('')
     const { connectedToSocket } = useContext(RoomContext);
-    const { id } = useParams();
-    const navigate = useNavigate();
 
-    useEffect(() => {
-        if(id)  navigate(`/room/${id}`)
-    }, [])
-    
     return (
         <div className="App flex flex-col items-center justify-center w-screen h-screen bg-gradient-to-t from-orange-600 to-amber-500">
             <Logo className="mb-20" />
@@ -22,13 +14,11 @@ const Home = () => {
             {!connectedToSocket && (
                 <div className="flex flex-col items-center">
                     <Spinner />
-                    <h2 className="py-8 text-white">Please allow a few seconds to connect to the server...</h2>
+                    <h2 className="py-8 text-white">
+                        Please allow a few seconds to connect to the server...
+                    </h2>
                 </div>
             )}
-            <div>
-                <input type='text' onChange={(e) => setRoomId(e.target.value)} value={roomId}/>
-                <button onClick={() =>  navigate(`/room/${roomId}`)}>Join Room</button>
-            </div>
         </div>
     );
 };
